0c63e9110f641fb19ffd18c61920a0562d46aadb
Digital Solutioner <48715320+carlovarchetta@users.noreply.github.com>
2022-01-11 15:01:05 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
+// const initApi = (req) => {
+//   return Prismic.client(process.env.PRISMIC_ENDPOINT, {
+//     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+//     req
+//   })
+// }
+
+const handleLinkResolver = doc => {
+  // // Define the url depending on the document type
+  // if (doc.type === 'page') {
+  //   return '/page/' + doc.uid;
+  // } else if (doc.type === 'blog_post') {
+  //   return '/blog/' + doc.uid;
+  // }
+
+  // Default to homepage
+  return '/'
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+
+  res.locals.PrismicDOM = PrismicDOM
+
+  next()
+})
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', async (req, res) => {
+  res.render('pages/home')
+})
+
+app.get('/about', async (req, res) => {
+  initApi(req).then(api => {
+    api.query(Prismic.Predicates.any('document.type', 'about')).then(response => {
+      const { results } = response
+      const [about, meta] = results
+
+      res.render('pages/about', {
+        about,
+        meta
+      })
+    })
+  })
+})
+
+app.get('/collection', (req, res) => {
+  res.render('pages/collection')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
